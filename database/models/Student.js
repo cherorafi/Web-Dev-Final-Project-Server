7bb55da2540e8e24eac1e7aa85431712367ce41e
cherorafi <saidun2001@gmail.com>
2023-05-11 16:35:38 -0400

    update models
    setup new fields for campuses and students. changed the values for creating the default students

--- database/models/Student.js
+++ database/models/Student.js
@@ -7,6 +7,13 @@ const Sequelize = require('sequelize');  // Import Sequelize
 const db = require('../db');  // Import Sequelize database instance called "db"
 
 const Student = db.define("student", {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
   firstname: {
     type: Sequelize.STRING,
     allowNull: false
@@ -15,6 +22,29 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  image: {
+    type:Sequelize.STRING,
+    defaultValue: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+  },
+
+  campusId: {
+    type: Sequelize.INTEGER,
+    allowNull: true
+  },
+
+  gpa: {
+    type: Sequelize.FLOAT,
+    validate: {
+      min: 0,
+      max: 4
+    }
   }
 });
 
